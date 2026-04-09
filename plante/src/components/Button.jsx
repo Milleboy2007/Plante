0a74a35c3f7c294fbfb93402502c95e100aeb40c
Nitro <2484435@cegepmv.ca>
2026-04-09 11:23:38 -0400

    clean up

--- plante/src/components/Button.jsx
+++ plante/src/components/Button.jsx
@@ -1,19 +1,6 @@
-import { useEffect, useState } from 'react'
 import './button.css'
 
 function Button(){
-
-    // const [isChanged, setIsChanged] = useState(false)
-
-    // useEffect(() =>{
-    //     try{
-    //         fetch(`http://localhost:3001/api/pump`, {
-    //             method:'POST'
-    //         }).catch(console.log("POST FAIL"))
-    //     }catch{
-    //         console.log("POST FAIL")
-    //     }
-    // }, [isChanged])
 
     function buttonClick(){
         try{
